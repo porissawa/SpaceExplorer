fc237b76e088ae39afe2fda7ba739b3b29573a49
porissawa <porissawa@users.noreply.github.com>
2019-01-24 18:08:58 -0200

    fixed styling and media types when querying api

--- index.js
+++ index.js
@@ -15,15 +15,19 @@ function generateRndNumber(min, max) {
 
 async function getData() {
     searchQuery = currentQuery.value
+
     //get data from nasa image api
     let res = await fetch(`https://images-api.nasa.gov/search?q=${searchQuery}`)
     let data = await res.json()
+
     //determine how many pages there are for the query
     let numberOfPages = Math.ceil(data.collection.metadata.total_hits / 100)
     let rndPage = Math.floor(Math.random() * (numberOfPages - 1) + 1)
+
     //fetch new data with randomized page
     res = await fetch(`https://images-api.nasa.gov/search?q=${searchQuery}&page=${rndPage}`)
     data = await res.json()
+
     //max entries per page
     const max = 99
     //generate random number as to select which description + image combo to show
@@ -31,7 +35,12 @@ async function getData() {
     //get nasa_id param to use on image URL
     let imgLink = data.collection.items[rnd].data[0].nasa_id
 
-    //insert description and image into DOM
-    document.getElementById('text_output').innerHTML = data.collection.items[rnd].data[0].description
-    document.getElementById('img_output').src = `http://images-assets.nasa.gov/image/${imgLink}/${imgLink}~orig.jpg`
+    if(data.collection.items[rnd].data[0].media_type !== 'image') {
+        getData()
+    } else {
+        //insert description and image into DOM
+        document.getElementById('image_title').innerHTML = data.collection.items[rnd].data[0].title
+        document.getElementById('text_output').innerHTML = data.collection.items[rnd].data[0].description
+        document.getElementById('img_output').src = `http://images-assets.nasa.gov/image/${imgLink}/${imgLink}~orig.jpg`
+    }
 }
